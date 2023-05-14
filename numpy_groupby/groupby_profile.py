e84427c7292db7f8b911f7812ee787a2ffefa1b7
Vikram <vikramsingh001@gmail.com>
2023-05-14 23:48:37 +0200

    Add pandas and numpy functions

--- numpy_groupby/groupby_profile.py
+++ numpy_groupby/groupby_profile.py
@@ -1,9 +1,13 @@
 import random
+import timeit
+
+import numpy as np
 import pandas as pd
 
 _CATEGORIES = ["red", "green", "blue"]
-_YEARS = range(1990, 2021)
+_YEARS = range(2010, 2021)
 _X_VALUES = [0, 0.1, 0.25, 0.5, 1.0]
+_INTERPOLATE_AT = 0.3
 
 
 def create_dataframe() -> pd.DataFrame:
@@ -17,6 +21,59 @@ def create_dataframe() -> pd.DataFrame:
     return pd.DataFrame(data, columns=["category", "year", "x", "y"])
 
 
+def _interpolate_wrapper(fp: np.ndarray, xp: np.ndarray, x: float) -> float:
+    return float(np.interp(x=x, xp=xp, fp=fp))
+
+
+def numpy_groupby(df: pd.DataFrame) -> pd.DataFrame:
+    categories = df["category"].to_numpy()
+    years = df["year"].to_numpy()
+    x_values = df["x"].to_numpy()
+    y_values = df["y"].to_numpy()
+
+    sort_indices = np.lexsort((x_values, years, categories))
+    y_values = y_values[sort_indices]
+
+    x_unique_values = np.unique(x_values)
+    num_x_unique_values = len(x_unique_values)
+
+    y_values = y_values.reshape([-1, num_x_unique_values])
+    interpolated_y_values = np.apply_along_axis(
+        _interpolate_wrapper,
+        axis=1,
+        arr=y_values,
+        x=_INTERPOLATE_AT,
+        xp=x_unique_values,
+    )
+
+    return pd.DataFrame(
+        data={
+            "category": categories.reshape([-1, num_x_unique_values])[:, 0],
+            "year": years.reshape([-1, num_x_unique_values])[:, 0],
+            "y": interpolated_y_values,
+        }
+    )
+
+
+def pandas_groupby(df: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df.groupby(["category", "year"])
+        .apply(lambda df: np.interp(_INTERPOLATE_AT, df["x"], df["y"]))
+        .rename("y")
+        .reset_index()
+    )
+
+
 if __name__ == "__main__":
-    df = create_dataframe()
-    print(df)
+    numpy_times = timeit.repeat(
+        "numpy_groupby(df)",
+        "from __main__ import create_dataframe, numpy_groupby;df = create_dataframe();",
+        number=100,
+    )
+    print(f"Numpy times: {numpy_times}")
+    pandas_times = timeit.repeat(
+        "pandas_groupby(df)",
+        "from __main__ import create_dataframe, pandas_groupby;df = create_dataframe()",
+        number=100,
+    )
+    print(f"Pandas times: {pandas_times}")
