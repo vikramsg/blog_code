380675d69888273847bc885c2c078061c9d0fe70
Vikram <vikramsingh001@gmail.com>
2023-05-20 23:15:53 +0200

    Add first go at creating routes

---
+++ deutschland_ticket/src/destinations.py
@@ -0,0 +1,46 @@
+from typing import List
+import requests
+
+from src.model import StopDeparturesResponseModel, TravelRoute
+
+
+def get_departures(stop_id: int) -> List[TravelRoute]:
+    url = (
+        f"https://v6.db.transport.rest/stops/{stop_id}/departures?"
+        "duration=120&bus=false&national=false&nationalExpress=false&suburban=false&subway=false&when=2023-05-20T08:00"
+    )
+
+    response = requests.get(url)
+    json_response = response.json()
+
+    # Parse and validate the JSON response using the ResponseModel
+    response_model = StopDeparturesResponseModel.parse_obj(json_response)
+
+    # Access the data from the response model
+    departures = response_model.departures
+
+    travel_routes = set()
+    # Print the first departure's tripId as an example
+    for departure in departures:
+        orig_name = departure.stop.name
+        orig_id = departure.stop.id
+        line = departure.line
+        line_name = line.name
+        destination_name = departure.destination.name
+        destination_id = departure.destination.id
+
+        travel_route = TravelRoute(
+            origin=orig_name,
+            origin_id=orig_id,
+            destination=destination_name,
+            destination_id=destination_id,
+            train_line=line_name,
+        )
+        travel_routes.add(travel_route)
+
+    return list(travel_routes)
+
+
+if __name__ == "__main__":
+    hamburg_stop_id = 8002549
+    get_departures(hamburg_stop_id)
---
+++ deutschland_ticket/src/model.py
@@ -0,0 +1,99 @@
+from typing import Any, List, Optional
+
+from pydantic import BaseModel
+
+
+class Location(BaseModel):
+    type: str
+    id: str
+    latitude: float
+    longitude: float
+
+
+class Products(BaseModel):
+    nationalExpress: bool
+    national: bool
+    regionalExpress: bool
+    regional: bool
+    suburban: bool
+    bus: bool
+    ferry: bool
+    subway: bool
+    tram: bool
+    taxi: bool
+
+
+class Stop(BaseModel):
+    type: str
+    id: str
+    name: str
+    location: Location
+    products: Products
+
+
+class Operator(BaseModel):
+    type: str
+    id: str
+    name: str
+
+
+class Line(BaseModel):
+    type: str
+    id: str
+    fahrtNr: str
+    name: str
+    public: bool
+    adminCode: str
+    productName: str
+    mode: str
+    product: str
+    operator: Operator
+    additionalName: str
+
+
+class Destination(BaseModel):
+    type: str
+    id: str
+    name: str
+    location: Location
+    products: Products
+
+
+class Departure(BaseModel):
+    tripId: str
+    stop: Stop
+    when: str
+    plannedWhen: str
+    delay: Optional[str]
+    platform: str
+    plannedPlatform: str
+    prognosisType: Optional[str]
+    direction: str
+    provenance: Optional[str]
+    line: Line
+    remarks: List[str]
+    origin: Optional[str]
+    destination: Destination
+
+
+class StopDeparturesResponseModel(BaseModel):
+    departures: List[Departure]
+    realtimeDataUpdatedAt: int
+
+
+class TravelRoute(BaseModel, set):
+    origin: str
+    destination: str
+    train_line: str
+    origin_id: int
+    destination_id: int
+
+    def __hash__(self) -> int:
+        # Override hash to consider only origin and destination fields
+        return hash((self.origin, self.destination))
+
+    def __eq__(self, other: Any) -> bool:
+        # Override equality to consider only origin and destination fields
+        if isinstance(other, TravelRoute):
+            return self.origin == other.origin and self.destination == other.destination
+        return False
